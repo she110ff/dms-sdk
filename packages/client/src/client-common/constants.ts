ee72d8efd066cb2be9ade7a46dc0333175b66377
Kang-Simon <sangyoon.kang@bosagora.io>
2023-08-21 13:08:31 +0900

    Update dms-osx library

--- packages/client/src/client-common/constants.ts
+++ packages/client/src/client-common/constants.ts
@@ -2,28 +2,28 @@ import { NetworkDeployment, SupportedNetworks } from "./interfaces/common";
 import { activeContractsList } from "dms-osx-lib";
 
 export const LIVE_CONTRACTS: { [K in SupportedNetworks]: NetworkDeployment } = {
-    mainnet: {
-        LinkCollection: activeContractsList.devnet.LinkCollection,
-        Token: activeContractsList.devnet.Token,
-        ValidatorCollection: activeContractsList.devnet.ValidatorCollection,
-        TokenPrice: activeContractsList.devnet.TokenPrice,
-        FranchiseeCollection: activeContractsList.devnet.FranchiseeCollection,
-        Ledger: activeContractsList.devnet.Ledger
+    bosagora_mainnet: {
+        LinkCollection: activeContractsList.bosagora_mainnet.LinkCollection,
+        Token: activeContractsList.bosagora_mainnet.Token,
+        ValidatorCollection: activeContractsList.bosagora_mainnet.ValidatorCollection,
+        TokenPrice: activeContractsList.bosagora_mainnet.TokenPrice,
+        FranchiseeCollection: activeContractsList.bosagora_mainnet.FranchiseeCollection,
+        Ledger: activeContractsList.bosagora_mainnet.Ledger
     },
-    testnet: {
-        LinkCollection: activeContractsList.devnet.LinkCollection,
-        Token: activeContractsList.devnet.Token,
-        ValidatorCollection: activeContractsList.devnet.ValidatorCollection,
-        TokenPrice: activeContractsList.devnet.TokenPrice,
-        FranchiseeCollection: activeContractsList.devnet.FranchiseeCollection,
-        Ledger: activeContractsList.devnet.Ledger
+    bosagora_testnet: {
+        LinkCollection: activeContractsList.bosagora_testnet.LinkCollection,
+        Token: activeContractsList.bosagora_testnet.Token,
+        ValidatorCollection: activeContractsList.bosagora_testnet.ValidatorCollection,
+        TokenPrice: activeContractsList.bosagora_testnet.TokenPrice,
+        FranchiseeCollection: activeContractsList.bosagora_testnet.FranchiseeCollection,
+        Ledger: activeContractsList.bosagora_testnet.Ledger
     },
-    localhost: {
-        LinkCollection: activeContractsList.devnet.LinkCollection,
-        Token: activeContractsList.devnet.Token,
-        ValidatorCollection: activeContractsList.devnet.ValidatorCollection,
-        TokenPrice: activeContractsList.devnet.TokenPrice,
-        FranchiseeCollection: activeContractsList.devnet.FranchiseeCollection,
-        Ledger: activeContractsList.devnet.Ledger
+    bosagora_devnet: {
+        LinkCollection: activeContractsList.bosagora_devnet.LinkCollection,
+        Token: activeContractsList.bosagora_devnet.Token,
+        ValidatorCollection: activeContractsList.bosagora_devnet.ValidatorCollection,
+        TokenPrice: activeContractsList.bosagora_devnet.TokenPrice,
+        FranchiseeCollection: activeContractsList.bosagora_devnet.FranchiseeCollection,
+        Ledger: activeContractsList.bosagora_devnet.Ledger
     }
 };
